bf0259265c588c2dbf9d938c4dfaf313a7395608
uzma <ubkhan3@gmail.com>
2021-11-16 15:31:08 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //write your code here
+
+function replaceWithYerr(arr, start, num){
+  let newArray = [];
+ 
+ for(let i = start; i <= num; i++){
+      arr[i] = "yerr";
+     }
+ 
+   for(let i = 0; i < arr.length; i++){
+     newArray.push(arr[i]);
+  }
+
+ 
+  return newArray;
+}
